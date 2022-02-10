70e894fbb3d0dc02fed3b14bd2b02ba2f3d41810
yulechka.kudinova@mail.ru <yulechka.kudinova.1991@mail.ru>
2022-02-10 20:00:07 +0300

    third commit

--- src/main/java/ru/netology/task/Radio.java
+++ src/main/java/ru/netology/task/Radio.java
@@ -1,16 +1,20 @@
 package ru.netology.task;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+
+@Data
 public class Radio {
     private int currentRadioStation;
     private int numberStation = 10;
     private int currentVolume;
+    private int maxStation = maxStation();
 
-
-    public int getCurrentRadioStation() {
-        return currentRadioStation;
-    }
     public int maxStation() {
-        return (numberStation - 1);
+         return (numberStation - 1);
     }
 
     public Radio(int numberStation) {
@@ -20,46 +24,32 @@ public class Radio {
     public Radio() {
     }
 
-    public void setCurrentRadioStation(int currentRadioStation) {
+    public void nextRadioStation() {
+        setCurrentRadioStation(currentRadioStation + 1);
         if (currentRadioStation > maxStation()) {
             currentRadioStation = 0;
         }
-        if (currentRadioStation < 0) {
-            currentRadioStation = maxStation();
-        }
-        this.currentRadioStation = currentRadioStation;
-    }
-
-    public void nextRadioStation() {
-        setCurrentRadioStation(currentRadioStation + 1);
     }
 
     public void prevRadioStation() {
         setCurrentRadioStation(currentRadioStation - 1);
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > 100) {
-            currentVolume = 100;
+        if (currentRadioStation < 0) {
+            currentRadioStation = maxStation();
         }
-        if (currentVolume < 0) {
-            currentVolume = 0;
-        }
-        this.currentVolume = currentVolume;
     }
 
     public void increaseVolume() {
-           setCurrentVolume(currentVolume + 1);
+        setCurrentVolume(currentVolume + 1);
+        if (currentVolume > 100) {
+            currentVolume = 100;
         }
-
+    }
 
     public void reduceVolume() {
         setCurrentVolume(currentVolume - 1);
+        if (currentVolume < 0) {
+            currentVolume = 0;
+        }
     }
-
 }
 
